ca8cc1c8710870a339e27fd5d4fffd221c41e27e
Charismatic30 <132722351+Charismatic30@users.noreply.github.com>
2026-02-09 23:39:57 +0900

    javascript for show and hid button added

--- script.js
+++ script.js
@@ -2,14 +2,41 @@ const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
 
+
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
   },
+  
 
   breakpoints: {
-    375: {
+    320: {
       enabled: false,
     }
   }
 });
+
+const showBtn = document.getElementById('showButton');
+const hideBtn = document.getElementById('hideButton');
+const bottomItems = document.querySelectorAll('.hide');
+
+function hideBottomGrid(show) {
+  bottomItems.forEach(item => {
+    if (show) {
+      item.classList.remove('hide');
+    } else {
+      item.classList.add('hide');
+    }
+  });
+
+  showBtn.style.display = show ? 'none' : 'flex';
+  hideBtn.style.display = show ? 'flex' : 'none';
+}
+
+showBtn.addEventListener('click', () => hideBottomGrid(true));
+hideBtn.addEventListener('click', () => hideBottomGrid(false));
+
+
+
+
+ 
